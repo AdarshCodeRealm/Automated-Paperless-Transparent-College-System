d83177c76e59de6599e700c2f36d442768855a98
adarsh ramgirwar <adarshramgirwar1@gmail.com>
2025-02-22 12:34:59 +0530

    cheating routes set

--- backend/src/controllers/integrityAndCheatingRecord.controller.js
+++ backend/src/controllers/integrityAndCheatingRecord.controller.js
@@ -1,2 +1,44 @@
 // 5. Academic Integrity & Cheating Record System
 // ● If a stude nt is caught cheating in an exam, their details (name, reason, and proof) are publicly visible to all students.
+
+// Create a new violation record
+
+import Violation from "../models/cheatingRecord.model.js"
+const createViolation = async (req, res) => {
+  try {
+    const violation = new Violation(req.body)
+    await violation.save()
+    res.status(201).json(violation)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+// Get all violation records
+
+const getAllViolations = async (req, res) => {
+  try {
+    const violations = await Violation.find()
+    res.json(violations)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Delete a violation record by ID
+
+const deleteViolation = async (req, res) => {
+  try {
+    const violation = await Violation.findByIdAndDelete(req.params.id)
+    if (!violation) {
+      return res.status(404).json({ message: "Violation not found" })
+    }
+    res.json({ message: "Violation deleted" })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+export {
+  createViolation,
+  getAllViolations,
+  deleteViolation,}
--- backend/src/routes/integrityAndCheatingRecord.routes.js
+++ backend/src/routes/integrityAndCheatingRecord.routes.js
@@ -1,15 +1,22 @@
-import { Router } from 'express';
-import testModel from '../models/test.model.js';
-const router = Router();
+import { Router } from "express"
+
+import {
+  createViolation,
+  getAllViolations,
+  deleteViolation,
+} from "../controllers/integrityAndCheatingRecord.controller.js"
+const router = Router()
 
 // router.get("/test", functionName imported from controller file);
-router.get("/test",async (req, res) => {
-    
-    res.status(200).json({
-        status: "success",
-        message: "integrityAndCheatingRecord route"
-    })
+router.get("/test", async (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "integrityAndCheatingRecord route",
+  })
 })
 
+router.post("/", createViolation)
+router.get("/", getAllViolations)
+router.delete("/:id", deleteViolation)
 
 export default router
