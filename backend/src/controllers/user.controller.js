18c9a6388046d818fd9949296c79d14942752ca2
adarsh ramgirwar <adarshramgirwar1@gmail.com>
2025-02-22 09:04:14 +0530

    college validation department and role idenfy logic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -32,7 +32,39 @@ const generateAccessAndRefereshTokens = async (userId) => {
 }
 
 const registerUser = async (req, res) => {
-  const { name, email, password, role, mobile, bio, department } = req.body
+  const { name, email, password, mobile, bio } = req.body
+
+  if (typeof email !== "string") {
+    return false // Handle cases where email is not a string
+  }
+  const validClgMail = email.toLowerCase().endsWith("@sggs.ac.in")
+  if (!validClgMail) {
+    return res.status(400).json({
+      status: "Not valid email address, please use college mail id",
+      message: `Invalid email : ${email}`,
+    })
+  }
+  const emailLower = email.toLowerCase()
+  const department = emailLower.includes("bcs")
+    ? "Computer Science"
+    : emailLower.includes("bec")
+      ? "Electronics and Communication"
+      : emailLower.includes("bme")
+        ? "Mechanical Engineering"
+        : emailLower.includes("bce")
+          ? "Civil Engineering"
+          : emailLower.includes("bec")
+            ? "Electrical and Electronics Engineering"
+            : emailLower.includes("bit")
+              ? "Information Technology"
+              : null // Default: unknown department
+
+  let role
+  if (department == null) {
+    role = "Professor"
+  } else {
+    role = "Student"
+  }
 
   const checkUserIsExist = await userModel.findOne({ email })
   if (checkUserIsExist) {
@@ -160,7 +192,6 @@ const loginUser = async (req, res) => {
   }
 
   const isMatch = await user.isPasswordCorrect(password)
- 
 
   if (!isMatch) {
     return res.status(400).json({
