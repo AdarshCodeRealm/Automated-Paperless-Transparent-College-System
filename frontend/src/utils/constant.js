4020cf1759a219b8d5f9ecaf84696cec6b45f761
adarsh ramgirwar <adarshramgirwar1@gmail.com>
2025-03-04 16:12:13 +0530

    authentication done need to improve some load

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
-// const backend_URL = 'https://hackfusion-2025.onrender.com'
-const backend_URL = 'http://localhost:5000'
+const backend_URL = 'https://hackfusion-2025.onrender.com'
+// const backend_URL = 'http://localhost:5000'
 export {backend_URL}
